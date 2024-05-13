b36bf4fe9a40e7288d12c451e986ae8e68428afc
AlvaroGR04 <alvarogallardorodriguez791@gmail.com>
2024-05-13 10:46:08 +0200

    Comandos
    Tarea 18-20

---
+++ Comando.sh
@@ -0,0 +1,24 @@
+# Crear una nueva rama para la interfaz de usuario
+$ git checkout -b interfaz_usuario
+
+# Crear una nueva rama para la gestión de notas
+$ git checkout -b gestion_notas
+
+# Crear una nueva rama para la gestión de notas
+$ git checkout -b gestion_notas
+
+# Añadir los cambios al área de preparación
+$ git add .
+
+# Realizar un commit con un mensaje descriptivo
+$ git commit -m "Mensaje descriptivo del commit"
+
+# Actualiza tu rama main local
+$ git checkout main
+$ git pull origin main
+
+# Fusiona la rama de la característica aprobada con main
+$ git merge nombre_de_la_rama
+
+# Sube los cambios al repositorio remoto
+$ git push origin main
